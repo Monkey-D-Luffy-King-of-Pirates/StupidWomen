f226e412aa8b82966d6169885fa9fba3b792b734
Демон Чайный <volshebnuyoleg666@yandex.ru>
2023-07-14 19:17:45 +0400

    обязательно

--- StupidWomen/Program.cs
+++ StupidWomen/Program.cs
@@ -8,34 +8,33 @@ namespace StupidWomen
 {
     internal class StupidWomen
     {
-       
-
         static void Main(string[] args)
         {
-            
-
             int myNumber;
+
             Console.Write("Please, enter 1 or 2: ");
-            while (!int.TryParse(Console.ReadLine(), out myNumber))
+
+            while (!int.TryParse(Console.ReadLine(), out myNumber) || !(myNumber > 0 && myNumber < 3))
             {
                 Console.Clear();
                 Console.Write("No! \nPlease, enter 1 or 2: "); 
             }
+
                 if (myNumber == 1)
                 {
                     Console.WriteLine("Fuck off, stupid women");
 
                 }
+
                 else if (myNumber == 2)
                 {
                     Console.WriteLine("Shut up, stupid women");                   
                 }
-                else
-                {
-                    Console.WriteLine("Are you stupid?!");
-                }
+
                 Console.ReadKey();
+
                 Console.Clear();
+
                 Console.WriteLine("Please, enter 1 or 2: ");
         }
     }       
